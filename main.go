5fcbf9f92db31e660a27ce105d4c69679000756c
Emerson Pedroso <emerson@bitcoinnano.org>
2024-08-12 14:43:48 -0300

    fix

--- main.go
+++ main.go
@@ -11,14 +11,14 @@ import (
 
 func main() {
 	// Define a flag for the port
-	port := flag.Int("port", 8080, "Port to listen on")
+	port := flag.Int("port", 80, "Port to listen on")
 	flag.Parse()
 
 	// Create a new Gin router
 	router := gin.Default()
 	api.Register(router)
 
-	// Run the server on port 8080
+	// Run the server on port 80
 	err := router.Run(fmt.Sprintf(":%d", *port))
 	if err != nil {
 		panic(err)
